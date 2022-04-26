179e6c77b2ca739fa4dd18f21898f5580822963c
Allen <almcbroombey@hotmail.com>
2022-04-26 00:01:52 -0400

    created function for display current weather and 5-day forecast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,8 +4,7 @@ var currentWeatherEl = document.querySelector('.current-weather-div');
 var forecastEl = document.querySelector('.forecast-div');
 var searchEl = document.querySelector('.search-div');
 var APIkey = "ec41c2204e5399dfc35aa2f07e953c27";
-// this function is a call to get the weather on a user entered city and:
-// to get the lat and long of the entered city.
+// this function is a call to get the weather on a user entered city and to get the lat and long of the entered city. 
 function getAPI(cityName) {
     var apiGeoCoding = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + APIkey;
     var i = 0;
@@ -20,9 +19,8 @@ function getAPI(cityName) {
     })
         .then(function (locationData) {
             if (locationData) {
-                // GeoCoding API returns a max of 5 locations from the cityName provided so to get the index to use, compare the whole name of the output to the one entered
+                // GeoCoding API returns a total of 5 locations from the cityName user enters
                 while (i < locationData.length) {
-                    //if country code is entered, split the value and just use the first part to look for the approriate array to use in the next part
                     cityNameSplit = cityName.split(',');
                     cityName = (cityNameSplit.length > 1) ? cityNameSplit[0] : cityName;
                     if (locationData[i].name.toUpperCase() === cityName.toUpperCase()) {
@@ -50,18 +48,18 @@ function getAPI(cityName) {
 }
 
 function getWeather(api, cityName) {    
-    //moved function out of getAPI so that it can also be called when clicking the buttons
+    //function of getAPI to be called when clicking the button
     fetch(api).then(function (response) {
         if (response.ok) {
             response.json().then(function (weatherData) {
                 console.log(weatherData);
                 // display current weather
                 displayCurrentWeather(weatherData.current, cityName);
-                // use daily[1] to daily[6] from the API response to show the 5-day forecast
+                // API response to show the 5-day forecast
                 for (var i = 1; i < 6; i++){      
                     displayForecast(weatherData.daily[i],i);   
                 }
-                // create button for history
+                // shows the create button for history
                 createHistoryButton(cityName, api);
             })
         } else {
@@ -73,7 +71,7 @@ function getWeather(api, cityName) {
 
 function displayCurrentWeather(resultObj, cityName) {
     if (!currentWeatherEl.hasChildNodes()) {
-        //first time doing a search so create cards for the weather
+        //Creates the cards for the weather forecast
         var resultCard = document.createElement('div');
         resultCard.classList.add('card', 'bg-light', 'text-dark', 'm-3', 'p-3', 'col-sm-11');
 }
